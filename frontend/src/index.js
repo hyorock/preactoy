0dbf94c81d46a8bb85c26f8b4fb27b24fce861f3
hyotak.an <hyotak.an@gumiprimus.com>
2017-12-20 10:26:48 +0900

    index.html index.js 원상복구

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
 
-const rootElement = document.getElementById('root'); 
-ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>), rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
