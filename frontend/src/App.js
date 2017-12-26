a312fdf0a361df9ccac5a820c5ff3cda36860176
hyotak.an <hyotak.an@gumiprimus.com>
2017-12-26 18:05:50 +0900

    App class 로 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
@@ -11,25 +11,27 @@ import BaseballGame from './routes/BaseballGame';
 import Todolist from './routes/todolist/Todolist';
 import NoMatch from './routes/NoMatch';
 
-const App = () => {
-    return (
-        <Router>
-            <div>
-                <Header/>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about/:username" component={About} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/posts" component={Posts} />
-                    <Route path="/me" component={MyPage} />
-                    <Route path="/search" component={Search} />
-                    <Route path="/baseballgame" component={BaseballGame} />
-                    <Route path="/todolist" component={Todolist} />
-                    <Route component={NoMatch} />
-                </Switch>
-            </div>
-        </Router>
-    );
-};
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Header/>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about/:username" component={About} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/posts" component={Posts} />
+                        <Route path="/me" component={MyPage} />
+                        <Route path="/search" component={Search} />
+                        <Route path="/baseballgame" component={BaseballGame} />
+                        <Route path="/todolist" component={Todolist} />
+                        <Route component={NoMatch} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
 
 export default App;
